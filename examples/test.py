d1a94b9b93e22ac318c75411e3d1dc9eac843066
Josh Cohen <61619195+yecohn@users.noreply.github.com>
2025-06-19 15:12:41 +0300

    Update test.py

--- examples/test.py
+++ examples/test.py
@@ -1,4 +1,10 @@
+#
+# This file is part of TEN Framework, an open source project.
+# Licensed under the Apache License, Version 2.0.
+# See the LICENSE file for more information.
+#
 import sys, os
+import time
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../include")))
 from ten_vad import TenVad
@@ -6,16 +12,21 @@ import scipy.io.wavfile as Wavfile
 
 
 if __name__ == "__main__":
-    input_file, out_path = sys.argv[1], sys.argv[2]
+    input_file = sys.argv[1]
+    print(f"{input_file} processed ! ")
+    out_path = input_file.replace(".wav", "_ten_vad_pred.csv")
     sr, data = Wavfile.read(input_file)
-    hop_size = 256  # 16 ms per frame
+    hop_size = 500  # 16 ms per frame
     threshold = 0.5
     ten_vad_instance = TenVad(hop_size, threshold)  # Create a TenVad instance
     num_frames = data.shape[0] // hop_size
     # Streaming inference
     with open(out_path, "w") as f:
         for i in range(num_frames):
-            audio_data = data[i * hop_size: (i + 1) * hop_size]
-            out_probability, out_flag = ten_vad_instance.process(audio_data) #  Out_flag is speech indicator (0 for non-speech signal, 1 for speech signal)
-            print("[%d] %0.6f, %d" % (i, out_probability, out_flag))
-            f.write("[%d] %0.6f, %d\n" % (i, out_probability, out_flag))
+            audio_data = data[i * hop_size : (i + 1) * hop_size]
+            out_probability, out_flag, lag = ten_vad_instance.process(audio_data)
+            #  Out_flag is speech indicator (0 for non-speech signal, 1 for speech signal)
+            # print("[%d] %0.6f, %d" % (i, out_probability, out_flag))
+            f.write("{}, {}, {:.6f}\n".format(i, out_flag, lag))
+
+    print(f"{out_path} written ! ")
