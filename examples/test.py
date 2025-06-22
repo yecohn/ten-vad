18d990e2226f633997b5dfb2b6352c333491d00e
yehoshua <yehoshua.cohen.91@gmail.com>
2025-06-22 18:09:35 +0300

    insert dynamically sampling rate and number of sample per blob

--- examples/test.py
+++ examples/test.py
@@ -4,23 +4,34 @@
 # See the LICENSE file for more information.
 #
 import sys, os
+import time
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../include")))
 from ten_vad import TenVad
+import librosa
+import numpy as np
 import scipy.io.wavfile as Wavfile
 
-
 if __name__ == "__main__":
-    input_file, out_path = sys.argv[1], sys.argv[2]
+    input_file, sampling_rate, hop_size = (
+        sys.argv[1],
+        int(sys.argv[2]),
+        int(sys.argv[3]),
+    )
     sr, data = Wavfile.read(input_file)
-    hop_size = 256  # 16 ms per frame
+
     threshold = 0.5
     ten_vad_instance = TenVad(hop_size, threshold)  # Create a TenVad instance
     num_frames = data.shape[0] // hop_size
     # Streaming inference
-    with open(out_path, "w") as f:
-        for i in range(num_frames):
-            audio_data = data[i * hop_size: (i + 1) * hop_size]
-            out_probability, out_flag = ten_vad_instance.process(audio_data) #  Out_flag is speech indicator (0 for non-speech signal, 1 for speech signal)
-            print("[%d] %0.6f, %d" % (i, out_probability, out_flag))
-            f.write("[%d] %0.6f, %d\n" % (i, out_probability, out_flag))
+    start = time.time()
+    for i in range(num_frames):
+        audio_data = data[i * hop_size : (i + 1) * hop_size]
+        out_probability, out_flag = ten_vad_instance.process(
+            audio_data
+        )  #  Out_flag is speech indicator (0 for non-speech signal, 1 for speech signal)
+        print("[%d] %0.6f, %d" % (i, out_probability, out_flag))
+
+    end = time.time()
+    lag = end - start
+    print(f"latency:{lag}")
